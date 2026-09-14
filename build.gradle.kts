0b871c8150ff2cf8b8b861dd5c54a953f4192ef3
P2wn <racecarboy77@gmail.com>
2026-09-14 16:20:11 -0400

    Upgrade Kotlin build tooling past known security advisory

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    kotlin("jvm") version "2.3.21"
+    kotlin("jvm") version "2.4.20"
     id("com.gradleup.shadow") version "9.2.2"
 }
 
